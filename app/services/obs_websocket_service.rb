8aa4659a9f1b25c1abe1d9958cd3a3656cd9edae
Andrew Denta <andrew.denta@gmail.com>
2024-10-30 15:01:27 -0400

    switch obs scenes

--- app/services/obs_websocket_service.rb
+++ app/services/obs_websocket_service.rb
@@ -16,107 +16,68 @@ class ObsWebsocketService
   end
 
   def open_connection
-    Async::WebSocket::Client.connect(@endpoint) do |connection|
-      scene_name = GAMEPLAY_SCENE
+    Async do |task|
+      Async::WebSocket::Client.connect(@endpoint) do |connection|
+        task.async do |subtask|
+          scene = GAMEPLAY_SCENE
+          loop do
+            subtask.sleep 2
 
-      challange_message = connection.read.to_h
+            scene_payload = {
+              "op": 6,
+              "d": {
+                "requestType": 'SetCurrentProgramScene',
+                "requestId": SecureRandom.uuid,
+                "requestData": {
+                  "sceneName": scene
+                }
+              }
+            }
 
-      challenge = challange_message[:d][:authentication][:challenge]
-      salt = challange_message[:d][:authentication][:salt]
+            scene_message = Protocol::WebSocket::TextMessage.generate(scene_payload)
+            scene_message.send(connection)
+            connection.flush
 
-      # Create authentication string
-      sha256 = OpenSSL::Digest.new('SHA256')
-      password_salt = ENV['OBS_WEBSOCKET_PASSWORD'] + salt
-      hash1 = sha256.digest(password_salt)
-      base64_secret = Base64.strict_encode64(hash1)
-      secret_challenge = base64_secret + challenge
-      hash2 = sha256.digest(secret_challenge)
-      authentication_string = Base64.strict_encode64(hash2)
+            scene = scene == GAMEPLAY_SCENE ? PAUSE_SCENE : GAMEPLAY_SCENE
+          end
+        end
 
-      auth_payload = {
-        'op' => 1,
-        'd' => {
-          'rpcVersion' => 1,
-          'authentication' => authentication_string
+        challange_message = connection.read.to_h
+
+        challenge = challange_message[:d][:authentication][:challenge]
+        salt = challange_message[:d][:authentication][:salt]
+
+        # Create authentication string
+        sha256 = OpenSSL::Digest.new('SHA256')
+        password_salt = ENV['OBS_WEBSOCKET_PASSWORD'] + salt
+        hash1 = sha256.digest(password_salt)
+        base64_secret = Base64.strict_encode64(hash1)
+        secret_challenge = base64_secret + challenge
+        hash2 = sha256.digest(secret_challenge)
+        authentication_string = Base64.strict_encode64(hash2)
+
+        auth_payload = {
+          'op' => 1,
+          'd' => {
+            'rpcVersion' => 1,
+            'authentication' => authentication_string
+          }
         }
-      }
 
-      begin
-        auth_message = Protocol::WebSocket::TextMessage.generate(auth_payload)
-        auth_message.send(connection)
-        connection.flush
-      rescue StandardError => e
-        puts e
+        begin
+          auth_message = Protocol::WebSocket::TextMessage.generate(auth_payload)
+          auth_message.send(connection)
+          connection.flush
+        rescue StandardError => e
+          puts e
+        end
+
+        while message = connection.read
+          puts message.to_h
+        end
+      ensure
+        task&.stop
       end
-
-      while message = connection.read
-        puts message.to_h
-      end
-
-      # loop do
-      #   task.sleep 5
-
-      #   puts 'switching scenes'
-
-      #   scene_payload = {
-      #     'op' => 6,
-      #     'd' => {
-      #       'requestType' => 'SetCurrentProgramScene',
-      #       'requestData' => { 'sceneName' => scene_name }
-      #     }
-      #   }
-
-      #   scene_message = Protocol::WebSocket::TextMessage.generate(scene_payload)
-      #   scene_message.send(connection)
-      #   connection.flush
-
-      #   scene_name = scene_name == GAMEPLAY_SCENE ? PAUSE_SCENE : GAMEPLAY_SCENE
-      # rescue StandardError => e
-      #   puts e
-      # end
     end
   end
 end
-
-# # OBS WebSocket server details from ENV variables
-# OBS_SERVER_ADDRESS = ENV['OBS_SERVER_ADDRESS']
-# OBS_SERVER_PASSWORD = ENV['OBS_SERVER_PASSWORD']
-
-# # Function to authenticate with the OBS server
-# def authenticate(ws)
-#   auth_payload = {
-#     'op' => 1,
-#     'd' => {
-#       'rpcVersion' => 1,
-#       'authentication' => OBS_SERVER_PASSWORD
-#     }
-#   }
-#   ws.send_text(auth_payload.to_json)
-# end
-
-# # Function to switch scenes
-# def switch_scene(ws, scene_name)
-#   payload = {
-#     'op' => 6,
-#     'd' => {
-#       'requestType' => 'SetCurrentProgramScene',
-#       'requestId' => rand(1..1000).to_s,
-#       'requestData' => { 'sceneName' => scene_name }
-#     }
-#   }
-#   ws.send_text(payload.to_json)
-# end
-
-# Async do
-#   Async::WebSocket::Client.connect(OBS_SERVER_ADDRESS) do |ws|
-#     authenticate(ws)
-
-#     current_scene = 'gameplay'
-
-#     loop do
-#       switch_scene(ws, current_scene)
-#       current_scene = current_scene == 'gameplay' ? 'pause' : 'gameplay'
-#       sleep 5 # Adjust delay as needed for toggling
-#     end
-#   end
-# end
--- lib/tasks/obs.rake
+++ lib/tasks/obs.rake
@@ -2,4 +2,8 @@ namespace :obs do
   task switch_scenes: :environment do
     ObsWebsocketService.new.open_connection
   end
+
+  task multi: :environment do
+    ObsWebsocketService.new.open_connection
+  end
 end
